0ee80c5b2f873e705ca970e3e25f4256be75d219
gozeberke <73718014+gozeberke@users.noreply.github.com>
2022-01-31 15:59:29 +0300

    template engine created

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express=require('express')
+const ejs=require('ejs')
 const app=express()
 
+//TEMPLATE ENGİNE
+app.set('view engine','ejs')
+
+
+// MIDDLEWARE
+app.use(express.static('public'))
+
+
+//ROUTES
 app.get('/',(req,res)=>{
-    const blog={
-        name:"CleanBlog",
-        description: "CleanBlog Description",
-    }
-    res.send(blog)
+   res.render('index')
 })
+app.get('/about',(req,res)=>{
+    res.render('about')
+ })
+ app.get('/add',(req,res)=>{
+    res.render('add_post')
+ })
 
 const port=3000
 app.listen(port,()=>{
